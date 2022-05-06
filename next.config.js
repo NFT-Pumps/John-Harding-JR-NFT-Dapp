e0719df5975e829416bd3959c59d4c63a9eba5f7
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-05-06 13:59:22 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     siteTitle: "The John Harding Jr Ticket Dapp",
     contractAddress: "0x65A12bc5cF30365DfEe396B50027FE0cAFDcd4FF",
     infuraID: "045e9c65387a4200aa8559c347fa19a4",
-    defaultGas: 150000,//75000,
+    defaultGas: 300000,//75000,
     mintType : "Pre-Sale", //Pre-Sale Public
     enforceWhitelist : true,
     GeneralAdmissionEth : "0.0",
